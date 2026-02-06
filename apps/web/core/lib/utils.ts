f8dcf48300b63a43eab343207215b0c41b4e95c9
mjbalcueva <75126648+mjbalcueva@users.noreply.github.com>
2026-02-06 16:55:44 +0800

    refactor(navigation): replace hardcoded API URL with getApiUrl utility function

--- apps/web/core/lib/utils.ts
+++ apps/web/core/lib/utils.ts
@@ -1,6 +1,16 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+import { env } from "@/env"
+
 export function cn(...inputs: ClassValue[]) {
 	return twMerge(clsx(inputs))
+}
+
+export function getAppUrl() {
+	return env.NEXT_PUBLIC_APP_URL
+}
+
+export function getApiUrl() {
+	return `${env.NEXT_PUBLIC_API_BASE_URL}/${env.NEXT_PUBLIC_API_VERSION}`
 }
